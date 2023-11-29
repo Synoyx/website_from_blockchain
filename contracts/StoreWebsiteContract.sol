513bcf4534afea4c5e1ca8053bb0af5d3cb1732e
Synoyx <synoyx@gmail.Com>
2023-11-30 00:03:39 +0100

    Added constructor to the contract, to make it works

--- contracts/StoreWebsiteContract.sol
+++ contracts/StoreWebsiteContract.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.8.2 <0.9.0;
 contract StoreWebsiteContract {
     string public websiteContent;
 
+    constructor() {
+    }
+
     function setWebsite(string memory _content) public {
         websiteContent = _content;
     }
