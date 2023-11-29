9f7ad2d6a74b5be182f8aaeea2043c2c1326b630
Synoyx <Synoyx@gmail.com>
2023-11-29 15:37:06 +0100

    Added some files for Remix execution

---
+++ contracts/StoreWebsiteContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract StoreWebsiteContract {
+    string public websiteContent;
+
+    function setWebsite(string memory _content) public {
+        websiteContent = _content;
+    }
+
+    function getWebsite() public view returns (string memory) {
+        return websiteContent;
+    }
+}
